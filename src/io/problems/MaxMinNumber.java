ec835b39e099bbe1b0ab1c7adfcfdd3a4aaad486
abdullah.samir <abdallahsameer22@gmail.com>
2024-05-26 14:56:31 +0300

    check Palindrome

--- src/io/problems/MaxMinNumber.java
+++ src/io/problems/MaxMinNumber.java
@@ -4,6 +4,8 @@ import io.problems.dtos.MaxMinDTO;
 public class MaxMinNumber {
     public static void main(String[] args) {
         System.out.println(getMaxMin(new int[] {100, 0, -100, 900, 50, 0}));
+
+        System.out.println(isPalindrome("ACCBFCA"));
     }
     private static MaxMinDTO getMaxMin(int [] data) {
         int min = data[0];
@@ -13,5 +15,16 @@ public class MaxMinNumber {
              max = Math.max(max, item);
         }
         return new MaxMinDTO(max, min);
+    }
+
+    private static boolean isPalindrome(String text) {
+        text = text.toLowerCase();
+        int tail = text.length() - 1;
+        for(int i = 0; i < text.length()/2; i++) {
+            if(text.charAt(i) != text.charAt(tail--)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
