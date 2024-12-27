63452c43b4645a74c543acf30592833aa4157130
Âbdallah Šamir <abdallahsameer22@gmail.com>
2024-12-27 16:45:11 +0200

    enhancements

--- src/java8/interfaces/App.java
+++ src/java8/interfaces/App.java
@@ -1,20 +1,20 @@
 package java8.interfaces;
 
-import java.util.List;
-import java.util.function.Function;
+import java.util.HashMap;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) {
 
-        // Functional Interface -> that contains only single abstract method (SAM).
 
-       List<String> ls = List.of("A", "B","C");
-       ls.forEach(System.out::println);
+        String info = "acbbcdaacea";
+        Map<Character, Integer> charOccurrences = new HashMap<>();
+
+
+
 
     }
 
-    private static void call(Executable executable) {
-        executable.print();
-    }
+
 }
 
