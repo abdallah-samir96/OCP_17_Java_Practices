a3ebf51304436794b2a43b8f37dca19897b5f688
Âbdallah Šamir <abdallahsameer22@gmail.com>
2025-02-01 15:23:17 +0200

    fail fast and fail safe in iterator

---
+++ src/problemsolving/jvm/App.java
@@ -0,0 +1,57 @@
+package problemsolving.jvm;
+
+import java.io.IOException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        System.out.println(Runtime.version());
+
+        // var process = Runtime.getRuntime().exec("calc");
+        //        Runtime.getRuntime().gc();
+        //        System.gc();
+
+        failSafe();
+    }
+
+    /**
+     * This method will trow ConcurrentModificationException
+     * because the iterator is fail fast when changing the collection when iterating on it
+     * lead to data consistency and error detects early
+     * */
+    static void failFast() {
+        List<String> animals = new ArrayList<>(List.of("Dog", "Cat", "Fox", "Lion"));
+        animals.forEach((animal)->{
+            animals.add("Another Animal " + animal);
+        });
+
+    }
+    /**
+     * This will work on copy and added the data on the original
+     * hard in debugging, data inconsistent
+     * */
+    static void failSafe() {
+        List<String> animals = new CopyOnWriteArrayList<>(List.of("Dog", "Cat", "Fox", "Lion"));
+        animals.forEach((animal)->{
+            animals.add("Another Animal " + animal);
+        });
+        System.out.println(animals);
+    }
+}
+
+
+
+class Test {
+    void display(int a) {
+        System.out.println("int: " + a);
+    }
+
+    void display(double a) {
+        System.out.println("double: " + a);
+    }
+}
