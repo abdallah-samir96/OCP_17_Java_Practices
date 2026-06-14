3d5740f0ac3ae3c9745c6faa143aad378eaac056
Abdallah Samir <abdallah01097261247@gmail.com>
2026-06-14 12:49:13 +0300

    enhamcements

--- src/lambda/expression/Main.java
+++ src/lambda/expression/Main.java
@@ -3,7 +3,18 @@ package lambda.expression;
 public class Main {
     public static void main(String[] args) {
 
+        Runnable runnable = ()->{
+            run();
+        };
+
+        new Thread(runnable).start();
 
 
+
+    }
+    private static void run(){
+
+        System.out.println(Runtime.getRuntime().availableProcessors());
+        System.out.println(Thread.currentThread().getName());
     }
 }
