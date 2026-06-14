19d55cf9ca18db7c5065c99c2f9259484d638a81
Abdallah Samir <abdallah01097261247@gmail.com>
2026-06-14 21:59:01 +0300

    enhancements

--- src/server/App.java
+++ src/server/App.java
@@ -10,15 +10,15 @@ import java.time.Instant;
 public class App {
     public static void main(String[] args) throws RemoteException, AlreadyBoundException, InterruptedException {
 
+        // This is RMI method invocation
         var service = new MessageServiceImpl();
         var registry = LocateRegistry.createRegistry(1000);
-
         registry.rebind("message_service", service);
-        System.out.println("Services is binded correctly!!");
+        System.out.println("Server is running now");
         while(true) {
             System.out.println("Time Is: " + Instant.now());
             Thread.sleep(3000);
         }
-       // System.out.println("Server is running now");
+
     }
 }
