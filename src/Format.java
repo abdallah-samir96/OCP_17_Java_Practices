5f4bc03431a498fe897d6178ab80f883b95513ad
Abdallah Samir <abdallah01097261247@gmail.com>
2026-08-04 12:40:33 +0300

    enhancement

---
+++ src/Format.java
@@ -0,0 +1,25 @@
+public class Format {
+    public static void main(String[] args) {
+
+        /**
+         *
+         * %s  -> String
+         * %d  -> Integer
+         * %f  -> Floating-point
+         * %.2f -> Floating-point (2 decimal places)
+         * %c  -> Character
+         * %b  -> Boolean
+         * %x  -> Hexadecimal
+         * %o  -> Octal
+         * %e  -> Scientific notation
+         * %%  -> Prints a percent sign (%)
+         * %n  -> New line
+         * %-10s -> Left-align in width 10
+         * %10d -> Right-align in width 10
+         * %05d -> Zero-pad integer to width 5
+         * %,d -> Thousands separator
+         * %+d -> Always show sign
+        **/
+
+    }
+}
