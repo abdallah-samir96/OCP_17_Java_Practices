c425bc9cce306bb5408c54d3f338888470404ec2
Abdallah Samir <abdallah01097261247@gmail.com>
2026-08-24 20:01:11 +0300

    enhancements

--- src/patterns/MainApp.java
+++ src/patterns/MainApp.java
@@ -1,17 +1,10 @@
 package patterns;
 
-import java.time.LocalDateTime;
 
 public class MainApp {
-    public static void main(String[] args) throws CloneNotSupportedException {
-        A.Test test = new A.Test(3, 4);
-        A a = new A(test, "Abdallah", LocalDateTime.now(), 1000, new StringBuilder("Hello"));
-        A b = (A)a.clone();
-        b.setName("Ahmed");
-        b.setStringBuilder(new StringBuilder("World"));
-        a.setStringBuilder(new StringBuilder("DDD"));
-        b.test.x = 1000;
-        System.out.println(a);
-        System.out.println(b);
+    public static void main(String[] args)  {
+        System.out.printf("Number of CPUs: %s %n" , Runtime.getRuntime().availableProcessors());
+        System.out.println(System.getenv());
+
     }
 }
